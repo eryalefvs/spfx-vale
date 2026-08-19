d41b0206d2cd6d2cf9d1564c41a7e1685f030bd0
eryalefvs <eryalef@gmail.com>
2026-08-19 18:37:13 -0300

    add file for 'Boa Jornada' constants

---
+++ src/constants/BoaJornadaConstants.ts
@@ -0,0 +1,26 @@
+export const SP_FIELDS_BOAJORNADA = {
+    BoaJornadaInfoGerais: {
+        Title: "Title",
+        Data: "Data",
+        Area: "Area",
+        Coordenacao: "Coordenacao",
+        Gerencia: "Gerencia",
+        Detalhamento: "Detalhamento"
+    },
+    BoaJornadaAtividade: {
+        Title: "Title",
+        JornadaId: "JornadaId",
+        Atividade: "Atividade",
+        Executantes: "Executantes",
+        OQuePodeMatar: "OQuePodeMatar",
+        RiscosCriticos: "RiscosCriticos",
+        RiscosMaiorIndice: "RiscosMaiorIndice",
+        Detalhamento: "Detalhamento"
+    },
+    Riscos: {
+        Title: "Title",
+        Descricao: "Descricao",
+        RACAssociado: "RAC_x0020_associado",
+        Icone: "Icone"
+    }
+} as const
