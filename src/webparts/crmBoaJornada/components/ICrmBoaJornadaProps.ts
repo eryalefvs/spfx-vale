5d232d1aaadc3d98d67723814003b82a9a37a8aa
eryalefvs <eryalef@gmail.com>
2026-08-19 21:35:44 -0300

    create webpart to Boa Jornada

---
+++ src/webparts/crmBoaJornada/components/ICrmBoaJornadaProps.ts
@@ -0,0 +1,6 @@
+export interface ICrmBoaJornadaProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ src/webparts/crmBoaJornada/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICrmBoaJornadaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CrmBoaJornadaWebPartStrings' {
+  const strings: ICrmBoaJornadaWebPartStrings;
+  export = strings;
+}
